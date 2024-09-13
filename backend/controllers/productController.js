a1247ac5254817d91f92ffffd0764fe13bf476c8
Shashi Kumar <shashikumar.g.242@gmail.com>
2024-09-13 18:03:01 +0530

    get-update-products-integrated

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,62 @@
-exports.getProducts = (req,res,next) => {
-    res.status(200).json({
-        success: true,
-        message : 'This route will show all products in database'
-    })
-}
+const Product = require("../models/product");
+
+//Create new Product   => api/v1/product/new
+exports.newProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// Get all products => /api/v1/products
+exports.getProducts = async (req, res, next) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    count: products.length,
+    products,
+  });
+};
+
+// Get Single Product details => /api/v1/product/:id
+
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// Update Product  => /api/v1/product/:id
+
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,11 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
+const {
+  getProducts,
+  newProduct,
+  getSingleProduct,
+  updateProduct,
+} = require("../controllers/productController");
 
-const { getProducts } = require('../controllers/productController');
+router.route("/products").get(getProducts);
 
+router.route("/product/:id").get(getSingleProduct);
 
-router.route('/products').get(getProducts);
+router.route("/admin/product/new").post(newProduct);
 
+router.route("/admin/product/:id").put(updateProduct);
 
 module.exports = router;
