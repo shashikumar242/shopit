82602304b805d517ee16a232242d1c195808e02d
Shashi Kumar <shashikumar.g.242@gmail.com>
2024-09-15 12:40:20 +0530

    backend-authentication-complete,protect-routes,forgotpassword-recovery-email,password-reset-option

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,87 +1,105 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please enter your name"],
+    maxLength: [30, "Your name cannot exceed 30 characters"],
+  },
 
-    name: {
-        type: String,
-        required: [true, 'Please enter your name'],
-        maxLength: [30,'Your name cannot exceed 30 characters']
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true,
+    validate: [validator.isEmail, "Please enter a valid email address"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minlength: [6, "Your password must be at least 6 characters"],
+    select: false,
+  },
+
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
     },
 
-    email: {
-        type: String,
-        required: [true, 'Please enter your email'],
-        unique: true,
-        validate: [validator.isEmail,'Please enter a valid email address']
+    url: {
+      type: String,
+      required: true,
     },
+  },
 
-    password: {
-        type: String,
-        required: [true, 'Please enter your password'],
-        minlength: [6, 'Your password must be at least 6 characters'],
-        select: false
-    },
+  role: {
+    type: String,
+    default: "user",
+  },
 
-    avatar: {
-        public_id: {
-            type: String,
-            required: true,
-        },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 
-        url: {
-            type: String,
-            required : true
-        }
-    },
-
-    role: {
-        type: String,
-        default : 'user'
-    },
-
-    createdAt: {
-        type: Date,
-        default: Date.now
-    },
-
-    resetPasswordToken: String,
-    resetPasswordExpire: Date
-
-
-})
-
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
+});
 
 // Encrypting password before saving
 
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) {
-        next()
-    }
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
 
-    this.password = await bcrypt.hash(this.password,10)
-})
-
+  this.password = await bcrypt.hash(this.password, 10);
+});
 
 // Compare user Password
 
 userSchema.methods.comparePassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password)
-}
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 // Return JWT token
 
 userSchema.methods.getJwtToken = function () {
-    return jwt.sign({
-        id: this._id,
+  return jwt.sign(
+    {
+      id: this._id,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRES_TIME,
+    },
+  );
+};
 
-    }, process.env.JWT_SECRET, {
-        expiresIn : process.env.JWT_EXPIRES_TIME
-    })
-}
+// Generate password reset token
 
+userSchema.methods.getResetPasswordToken = function () {
+  // Generate token
 
+  const resetToken = crypto.randomBytes(20).toString("hex");
 
-module.exports = mongoose.model('User', userSchema);
+  // Hash and set to resetPasswordToken
+
+  this.resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+
+  // Set token expire time
+
+  this.resetPasswordExpire = Date.now() + 30 * 60 * 1000;
+
+  return resetToken;
+};
+
+module.exports = mongoose.model("User", userSchema);
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -9,14 +9,17 @@ const {
   deleteProduct
 } = require("../controllers/productController");
 
-router.route("/products").get(getProducts);
+const { isAuthenticatedUser, authorizeRoles} = require('../middlewares/auth');
 
-router.route("/product/:id").get(getSingleProduct);
 
-router.route("/admin/product/new").post(newProduct);
+router.route("/products").get(isAuthenticatedUser,getProducts);
 
-router.route("/admin/product/:id").put(updateProduct);
+router.route("/product/:id").get(isAuthenticatedUser,getSingleProduct);
 
-router.route("/admin/product/:id").delete(deleteProduct)
+router.route("/admin/product/new").post(isAuthenticatedUser,authorizeRoles('admin'),newProduct); // bcas this are protected routes
+
+router.route("/admin/product/:id").put(isAuthenticatedUser,authorizeRoles('admin'),updateProduct);
+
+router.route("/admin/product/:id").delete(isAuthenticatedUser,authorizeRoles('admin'),deleteProduct)
 
 module.exports = router;
