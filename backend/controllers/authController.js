08b06b0ce89fb5c4bdb499d2f90d12cad18bb1c1
Shashi Kumar <shashikumar.g.242@gmail.com>
2024-09-15 15:05:02 +0530

    feat: added-admin-controlls, update,delete-user,user-update-details

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -135,6 +135,57 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
   sendToken(user, 200, res);
 });
 
+// Get currently logged in user details  => /api/v1/me
+
+exports.getuserProfile = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+// Update / Change password  => /api/v1/password/update
+
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+
+  // Check previous user password
+
+  const isMatched = await user.comparePassword(req.body.oldPassword);
+
+  if (!isMatched) {
+    return next(new ErrorHandler("Old password is incorrect"), 400);
+  }
+
+  user.password = req.body.password;
+  await user.save();
+
+  sendToken(user, 200, res);
+});
+
+// Update user profile => /api/v1/me/update
+
+exports.updateProfile = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  // Update avatar : TODO
+
+  const user = await User.findByIdAndUpdate(req.user.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
 // Logout user => api/v1/logout
 
 exports.logout = catchAsyncErrors(async (req, res, next) => {
@@ -146,5 +197,78 @@ exports.logout = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     success: true,
     message: "Logged Out",
+  });
+});
+
+////// Admin Routes //////
+
+// Get all users => /api/v1/users
+
+exports.allUsers = catchAsyncErrors(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    success: true,
+    users,
+  });
+});
+
+// Get user details => /api/v1/admin/user/:id
+
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User does not found with id:${req.params.id}`),
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+// Update user profile => /api/v1/admin/user/:id
+
+exports.updateUser = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role,
+  };
+
+  // Update avatar : TODO
+
+  const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// Delete User => /api/v1/admin/user/:id
+
+exports.deleteUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User does not found with id:${req.params.id}`),
+    );
+  }
+
+  // Remove avatar from clodinary - TODO
+
+  await user.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    user,
   });
 });
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
-const { registerUser,loginUser, logout, forgotPassword,resetPassword } = require('../controllers/authController');
+const { registerUser, loginUser, logout, forgotPassword, resetPassword, getuserProfile, updatePassword, updateProfile , allUsers,getUserDetails, updateUser, deleteUser} = require('../controllers/authController');
+
+const { isAuthenticatedUser,authorizeRoles} = require('../middlewares/auth');
 
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/password/forgot').post(forgotPassword);
 router.route('/password/reset/:token').put(resetPassword);
+router.route('/me').get(isAuthenticatedUser, getuserProfile);
+router.route('/password/update').put(isAuthenticatedUser, updatePassword);
+router.route('/me/update').put(isAuthenticatedUser, updateProfile)
 router.route('/logout').get(logout);
+
+
+router.route('/admin/users').get(isAuthenticatedUser, authorizeRoles('admin'), allUsers);
+router.route('/admin/user/:id').get(isAuthenticatedUser, authorizeRoles('admin'), getUserDetails);
+router.route('/admin/user/:id').put(isAuthenticatedUser, authorizeRoles('admin'), updateUser);
+router.route('/admin/user/:id').delete(isAuthenticatedUser, authorizeRoles('admin'), deleteUser);
+
+
+
+
 
 
 
